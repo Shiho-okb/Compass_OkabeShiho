f3a61e1ebe66eae4c041ba19661817e99842b24f
shiho_okabe <>
2024-12-27 22:11:46 +0900

    予約詳細ページにユーザー情報を表示する処理完了

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -1,31 +1,32 @@
-<x-sidebar>
-  <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
-    <div class="w-50 m-auto h-75">
-      <p>
-        <span>{{ $date }}日</span>
-        <span class="ml-3">{{ $part }}部</span>
-      </p>
-      <div class="h-75 border">
-        <table class="">
-          <tr class="text-center">
-            <th class="w-25">ID</th>
-            <th class="w-25">名前</th>
-            <th class="w-25">場所</th>
-          </tr>
-          <tbody>
-            <!-- 予約データに紐づくユーザー情報を表示 -->
-            @foreach($reservePersons as $reserve)
-            @foreach($reserve->users as $user)
+  <!-- スクール予約詳細画面 -->
+  <x-sidebar>
+    <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
+      <div class="w-50 m-auto h-75">
+        <p>
+          <span>{{ $date }}日</span>
+          <span class="ml-3">{{ $part }}部</span>
+        </p>
+        <div class="h-75 border">
+          <table class="">
             <tr class="text-center">
-              <td class="w-25">{{ $user->id }}</td>
-              <td class="w-25">{{ $user->over_name }} {{ $user->under_name }}</td>
-              <td class="w-25">リモート</td>
+              <th class="w-25">ID</th>
+              <th class="w-25">名前</th>
+              <th class="w-25">場所</th>
             </tr>
-            @endforeach
-            @endforeach
-          </tbody>
-        </table>
+            <tbody>
+              <!-- 予約データに紐づくユーザー情報を表示 -->
+              @foreach($reservePersons as $reserve)
+              @foreach($reserve->users as $user)
+              <tr class="text-center">
+                <td class="w-25">{{ $user->id }}</td>
+                <td class="w-25">{{ $user->over_name }} {{ $user->under_name }}</td>
+                <td class="w-25">リモート</td>
+              </tr>
+              @endforeach
+              @endforeach
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  </div>
-</x-sidebar>
+  </x-sidebar>
