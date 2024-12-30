fc77acccb7e15f178c6cd430a4fae4cd6cfefffb
shiho_okabe <>
2024-12-30 14:02:49 +0900

    新規ユーザー登録画面レイアウト完了

--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
-  <form action="{{ route('registerPost') }}" method="POST">
+  <form action="{{ route('registerPost') }}" method="POST" style="background-color: #ecf1f5; min-height: 100vh;">
     <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
-      <div class="w-25 vh-75 border p-3">
+      <div class="w-25 vh-75 border p-3 user_status" style="background-color: #fff; box-shadow: 0px 0px 15px -5px #777777;">
         <div class="register_form">
           <div class="d-flex mt-3" style="justify-content:space-between">
             <div class="" style="width:140px">
@@ -68,18 +68,24 @@
           </div>
         </div>
 
-        <div class="mt-3">
+        <div class="mt-3" style="display:flex; justify-content:space-between; padding: 0px 50px;">
           @if($errors->has('sex'))
           <div class="text-danger" style="font-size:12px;">
             {{ $errors->first('sex') }}
           </div>
           @endif
-          <input type="radio" name="sex" class="sex" value="1">
-          <label style="font-size:13px">男性</label>
-          <input type="radio" name="sex" class="sex" value="2">
-          <label style="font-size:13px">女性</label>
-          <input type="radio" name="sex" class="sex" value="3">
-          <label style="font-size:13px">その他</label>
+          <div>
+            <input type="radio" name="sex" class="sex" value="1">
+            <label style="vertical-align: middle; font-size:13px">男性</label>
+          </div>
+          <div>
+            <input type="radio" name="sex" class="sex" value="2">
+            <label style="vertical-align: middle; font-size:13px">女性</label>
+          </div>
+          <div>
+            <input type="radio" name="sex" class="sex" value="3">
+            <label style="vertical-align: middle; font-size:13px">その他</label>
+          </div>
         </div>
 
         <div class="mt-3">
@@ -89,87 +95,97 @@
           </div>
           @endif
           <label class="d-block m-0 aa" style="font-size:13px">生年月日</label>
-          <select class="old_year" name="old_year">
-            <option value="none">-----</option>
-            <option value="1985">1985</option>
-            <option value="1986">1986</option>
-            <option value="1987">1987</option>
-            <option value="1988">1988</option>
-            <option value="1989">1989</option>
-            <option value="1990">1990</option>
-            <option value="1991">1991</option>
-            <option value="1992">1992</option>
-            <option value="1993">1993</option>
-            <option value="1994">1994</option>
-            <option value="1995">1995</option>
-            <option value="1996">1996</option>
-            <option value="1997">1997</option>
-            <option value="1998">1998</option>
-            <option value="1999">1999</option>
-            <option value="2000">2000</option>
-            <option value="2001">2001</option>
-            <option value="2002">2002</option>
-            <option value="2003">2003</option>
-            <option value="2004">2004</option>
-            <option value="2005">2005</option>
-            <option value="2006">2006</option>
-            <option value="2007">2007</option>
-            <option value="2008">2008</option>
-            <option value="2009">2009</option>
-            <option value="2010">2010</option>
-          </select>
-          <label style="font-size:13px">年</label>
-          <select class="old_month" name="old_month">
-            <option value="none">-----</option>
-            <option value="01">1</option>
-            <option value="02">2</option>
-            <option value="03">3</option>
-            <option value="04">4</option>
-            <option value="05">5</option>
-            <option value="06">6</option>
-            <option value="07">7</option>
-            <option value="08">8</option>
-            <option value="09">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-          </select>
-          <label style="font-size:13px">月</label>
-          <select class="old_day" name="old_day">
-            <option value="none">-----</option>
-            <option value="01">1</option>
-            <option value="02">2</option>
-            <option value="03">3</option>
-            <option value="04">4</option>
-            <option value="05">5</option>
-            <option value="06">6</option>
-            <option value="07">7</option>
-            <option value="08">8</option>
-            <option value="09">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
-            <option value="21">21</option>
-            <option value="22">22</option>
-            <option value="23">23</option>
-            <option value="24">24</option>
-            <option value="25">25</option>
-            <option value="26">26</option>
-            <option value="27">27</option>
-            <option value="28">28</option>
-            <option value="29">29</option>
-            <option value="30">30</option>
-            <option value="31">31</option>
-          </select>
-          <label style="font-size:13px">日</label>
+          <div style="display:flex; justify-content:space-between; flex-wrap: nowrap;">
+            <div>
+              <select class="old_year border-bottom border-primary" name="old_year" style="border: none; padding-right: 30px;">
+                <option value="none">-----</option>
+                <option value="1985">1985</option>
+                <option value="1986">1986</option>
+                <option value="1987">1987</option>
+                <option value="1988">1988</option>
+                <option value="1989">1989</option>
+                <option value="1990">1990</option>
+                <option value="1991">1991</option>
+                <option value="1992">1992</option>
+                <option value="1993">1993</option>
+                <option value="1994">1994</option>
+                <option value="1995">1995</option>
+                <option value="1996">1996</option>
+                <option value="1997">1997</option>
+                <option value="1998">1998</option>
+                <option value="1999">1999</option>
+                <option value="2000">2000</option>
+                <option value="2001">2001</option>
+                <option value="2002">2002</option>
+                <option value="2003">2003</option>
+                <option value="2004">2004</option>
+                <option value="2005">2005</option>
+                <option value="2006">2006</option>
+                <option value="2007">2007</option>
+                <option value="2008">2008</option>
+                <option value="2009">2009</option>
+                <option value="2010">2010</option>
+              </select>
+              <label style="font-size:13px">年</label>
+            </div>
+
+            <div>
+              <select class="old_month border-bottom border-primary" name="old_month" style="border: none; padding-right: 30px;">
+                <option value="none">-----</option>
+                <option value="01">1</option>
+                <option value="02">2</option>
+                <option value="03">3</option>
+                <option value="04">4</option>
+                <option value="05">5</option>
+                <option value="06">6</option>
+                <option value="07">7</option>
+                <option value="08">8</option>
+                <option value="09">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+              </select>
+              <label style="font-size:13px">月</label>
+            </div>
+
+            <div>
+              <select class="old_day border-bottom border-primary" name="old_day" style="border: none; padding-right: 30px;">
+                <option value="none">-----</option>
+                <option value="01">1</option>
+                <option value="02">2</option>
+                <option value="03">3</option>
+                <option value="04">4</option>
+                <option value="05">5</option>
+                <option value="06">6</option>
+                <option value="07">7</option>
+                <option value="08">8</option>
+                <option value="09">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+                <option value="19">19</option>
+                <option value="20">20</option>
+                <option value="21">21</option>
+                <option value="22">22</option>
+                <option value="23">23</option>
+                <option value="24">24</option>
+                <option value="25">25</option>
+                <option value="26">26</option>
+                <option value="27">27</option>
+                <option value="28">28</option>
+                <option value="29">29</option>
+                <option value="30">30</option>
+                <option value="31">31</option>
+              </select>
+              <label style="font-size:13px">日</label>
+            </div>
+          </div>
         </div>
 
         <div class="mt-3">
@@ -179,14 +195,16 @@
           </div>
           @endif
           <label class="d-block m-0" style="font-size:13px">役職</label>
+        </div>
+        <div class="mt-1" style="display:flex; justify-content:space-between;">
           <input type="radio" name="role" class="admin_role role" value="1">
-          <label style="font-size:13px">教師(国語)</label>
+          <label style="font-size:13px; margin-bottom:0;">教師(国語)</label>
           <input type="radio" name="role" class="admin_role role" value="2">
-          <label style="font-size:13px">教師(数学)</label>
+          <label style="font-size:13px; margin-bottom:0;">教師(数学)</label>
           <input type="radio" name="role" class="admin_role role" value="3">
-          <label style="font-size:13px">教師(英語)</label>
+          <label style="font-size:13px; margin-bottom:0;">教師(英語)</label>
           <input type="radio" name="role" class="other_role role" value="4">
-          <label style="font-size:13px" class="other_role">生徒</label>
+          <label style="font-size:13px; margin-bottom:0;" class="other_role">生徒</label>
         </div>
 
         <div class="select_teacher d-none">
@@ -226,7 +244,8 @@
           <input type="submit" class="btn btn-primary register_btn" disabled value="新規登録" onclick="return confirm('登録してよろしいですか？')">
         </div>
         <div class="text-center">
-          <a href="{{ route('loginView') }}">ログイン</a>
+          <a href="{{ route('loginView') }}">ログインはこちら
+          </a>
         </div>
       </div>
       {{ csrf_field() }}
