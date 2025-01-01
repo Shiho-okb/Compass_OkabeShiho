05b6b794075f38fca4c46d1206d8ccadfe333f04
shiho_okabe <>
2025-01-01 14:04:03 +0900

    掲示板レイアウト途中まで完了

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -1,22 +1,26 @@
 <x-sidebar>
-  <div class="board_area w-100 border m-auto d-flex">
+  <div class="board_area w-100 m-auto d-flex">
     <div class="post_view w-75 mt-5">
-      <p class="w-75 m-auto">投稿一覧</p>
       @foreach($posts as $post)
       <div class="post_area border w-75 m-auto p-3">
-        <p><span>{{ $post->user->over_name }}</span><span class="ml-3">{{ $post->user->under_name }}</span>さん</p>
+        <p>
+          <!-- 苗字 -->
+          <span style="font-weight: 600; color: #999999;">{{ $post->user->over_name }}</span>
+          <!-- 名前 -->
+          <span class="ml-3" style="font-weight: 600; color: #999999;">{{ $post->user->under_name }}さん</span>
+        </p>
         <!-- 投稿タイトル表示 -->
-        <p><a href="{{ route('post.detail', ['id' => $post->id]) }}">{{ $post->post_title }}</a></p>
-        <!-- サブカテゴリー表示 -->
-        <!-- 投稿内容($post)からリレーションで紐付けたサブカテゴリーを取得 -->
-        @foreach($post->subCategories as $subCategory)
-        <span class="category_btn">{{ $subCategory->sub_category }}</span>
-        @endforeach
+        <p style="font-weight: 600; color: black;"><a class="title" href="{{ route('post.detail', ['id' => $post->id]) }}">{{ $post->post_title }}</a></p>
         <div class="post_bottom_area d-flex">
+          <!-- サブカテゴリー表示 -->
+          <!-- 投稿内容($post)からリレーションで紐付けたサブカテゴリーを取得 -->
+          @foreach($post->subCategories as $subCategory)
+          <span class="category_btn btn btn-info" style="background-color: #03AAD2;">{{ $subCategory->sub_category }}</span>
+          @endforeach
           <div class="d-flex post_status">
             <!-- コメント数 -->
             <div class="mr-5">
-              <i class="fa fa-comment"></i>
+              <i class="fa fa-comment" style="color: #999999;"></i>
               <span class="">{{ $post->postComments->count() }}</span>
             </div>
             <!-- いいね数 -->
@@ -37,36 +41,38 @@
         </div>
       </div>
       @endforeach
+
     </div>
 
 
     <!-- サイドバー -->
-    <div class="other_area border w-25">
-      <div class="border m-4">
+    <div class="other_area w-40" style="margin-top: 50px;">
+      <div class="">
         <!-- 投稿画面遷移ボタン -->
-        <div class=""><a href="{{ route('post.input') }}">投稿</a></div>
+        <div class="btn btn-info" style="background-color: #03AAD2; width: 100%;"><a class="post-input" href="{{ route('post.input') }}">投稿</a></div>
         <!-- 検索欄 -->
         <div class="">
           <input type="text" placeholder="キーワードを検索" name="keyword" form="postSearchRequest">
           <input type="submit" value="検索" form="postSearchRequest">
         </div>
-        <input type="submit" name="like_posts" class="category_btn" value="いいねした投稿" form="postSearchRequest">
-        <input type="submit" name="my_posts" class="category_btn" value="自分の投稿" form="postSearchRequest">
+        <input type="submit" name="like_posts" class="category_btn btn btn-info" style="background-color: #ea9dc7; border: none;" value="いいねした投稿" form="postSearchRequest">
+        <input type="submit" name="my_posts" class="category_btn btn btn-info" style="background-color: #e5bb62; border: none;" value="自分の投稿" form="postSearchRequest">
         <ul>
+          <span>カテゴリー検索</span>
           <!-- メインカテゴリー表示 -->
           @foreach($categories as $category)
           <li class="main_categories" category_id="{{ $category->id }}"><span>{{ $category->main_category }}<span></li>
-            <!-- サブカテゴリー表示 -->
-            @foreach($category->subCategories as $subCategory)
-            <li>
-              <!-- {{ $subCategory->id }} で各カテゴリーのid（データベースのユニークID）を値として設定。 -->
-              <!-- 例えば、カテゴリーIDが 1 の場合、value="1"となる。 -->
-              <form method="GET" action="{{ route('post.show') }}" id="subCategoryForm{{ $subCategory->id }}">
-                <input type="hidden" name="category_word" value="{{ $subCategory->id }}">
-                <button type="submit" class="category_btn">{{ $subCategory->sub_category }}</button>
-              </form>
-            </li>
-            @endforeach
+          <!-- サブカテゴリー表示 -->
+          @foreach($category->subCategories as $subCategory)
+          <li>
+            <!-- {{ $subCategory->id }} で各カテゴリーのid（データベースのユニークID）を値として設定。 -->
+            <!-- 例えば、カテゴリーIDが 1 の場合、value="1"となる。 -->
+            <form method="GET" action="{{ route('post.show') }}" id="subCategoryForm{{ $subCategory->id }}">
+              <input type="hidden" name="category_word" value="{{ $subCategory->id }}">
+              <button type="submit" class="category_btn">{{ $subCategory->sub_category }}</button>
+            </form>
+          </li>
+          @endforeach
           @endforeach
         </ul>
       </div>
