ffb05c68e433ab1364d45ec66dc074ed13476b78
shiho_okabe <>
2025-01-01 22:24:07 +0900

    プロフィール詳細画面レイアウト完了

--- resources/views/authenticated/users/profile.blade.php
+++ resources/views/authenticated/users/profile.blade.php
@@ -1,29 +1,29 @@
 <x-sidebar>
-  <div class="vh-100 border">
+  <div class="vh-100 border" style="padding: 10px;">
+    <span style="margin-left: 0.5rem; margin-right: 0.5rem;">{{ $user->over_name }}</span><span>{{ $user->under_name }}さんのプロフィール</span>
     <div class="top_area w-75 m-auto pt-5">
-      <span>{{ $user->over_name }}</span><span>{{ $user->under_name }}さんのプロフィール</span>
-      <div class="user_status p-3">
+      <div class="user_status p-3" style="box-shadow: 0px 0px 15px -5px #777777;">
         <p>名前 : <span>{{ $user->over_name }}</span><span class="ml-1">{{ $user->under_name }}</span></p>
         <p>カナ : <span>{{ $user->over_name_kana }}</span><span class="ml-1">{{ $user->under_name_kana }}</span></p>
         <p>性別 : @if($user->sex == 1)<span>男</span>@else<span>女</span>@endif</p>
         <p>生年月日 : <span>{{ $user->birth_day }}</span></p>
-        <div>選択科目 :
+        <div style="margin-bottom: 15px;">選択科目 :
           @foreach($user->subjects as $subject)
           <span>{{ $subject->subject }}</span>
           @endforeach
         </div>
         <div class="">
           @can('admin')
-          <span class="subject_edit_btn">選択科目の編集</span>
+          <span class="subject_edit_btn" style="font-size:16px; color: #05aad2;">選択科目の登録</span>
           <div class="subject_inner">
-            <form action="{{ route('user.edit') }}" method="post">
+            <form action="{{ route('user.edit') }}" method="post" style="display: flex; align-items: center;">
               @foreach($subjects as $subject)
               <div>
-                <label>{{ $subject->subject }}</label>
-                <input type="checkbox" name="subjects" value="{{ $subject->id }}">
+                <label style="margin-top: 0.5rem;">{{ $subject->subject }}</label>
+                <input style="margin-right: 10px;" type="checkbox" name="subjects" value="{{ $subject->id }}">
               </div>
               @endforeach
-              <input type="submit" value="編集" class="btn btn-primary">
+              <input type="submit" value="登録" class="btn btn-primary">
               <input type="hidden" name="user_id" value="{{ $user->id }}">
               {{ csrf_field() }}
             </form>
